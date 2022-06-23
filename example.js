a7edc2db3edc5e17c08342e2d0ea5940cf0af8e3
Caio Schmid <caioschmid@gmail.com>
2022-06-23 02:15:34 -0300

    Initial commit

---
+++ example.js
@@ -0,0 +1,22 @@
+const addon = require('./build/Release/addon');
+
+function jsSum() {
+  let a = 3.1415926,
+    b = 2.718;
+
+  for (let i = 0; i < 10000000000; i++) {
+    a += b;
+  }
+
+  let total = a;
+
+  return total;
+}
+
+console.time('c++');
+addon.sum();
+console.timeEnd('c++');
+
+console.time('js');
+// jsSum();
+console.timeEnd('js');
